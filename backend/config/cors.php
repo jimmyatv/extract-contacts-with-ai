acb93d5dacc04bb50537656ad540d2c1a021c790
jimmyatv <rasic.alexandar@gmail.com>
2025-06-20 11:35:13 +0200

    Add Vercel URL to allowed origins in CORS config

--- backend/config/cors.php
+++ backend/config/cors.php
@@ -5,7 +5,11 @@ return [
 
     'allowed_methods' => ['*'],
 
-    'allowed_origins' => ['http://localhost:3000'],
+    'allowed_origins' => [
+    'http://localhost:3000',
+    'https://my-laravel-app-382993121744.europe-west1.run.app',
+    'https://extract-contacts-with-ai.vercel.app',
+    ],
 
     'allowed_origins_patterns' => [],
 
